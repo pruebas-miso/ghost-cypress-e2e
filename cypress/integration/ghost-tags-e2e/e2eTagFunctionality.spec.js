753b39939e77282e4b23c099430f49c1118feaad
Pedro G <pedro3087@gmail.com>
2021-05-07 16:36:07 -0500

    4 scenarios - pending to add tag to page

---
+++ cypress/integration/ghost-tags-e2e/e2eTagFunctionality.spec.js
@@ -0,0 +1,52 @@
+/// <reference types="cypress" />
+
+import { TagsPage } from "../../page-objects/tags-page";
+import { DashboardPage } from "../../page-objects/dashboard-page";
+
+Cypress.on("uncaught:exception", (err, runnable) => {
+  return false;
+});
+
+describe("TAG Functionality - E2E scenrios", () => {
+  const tagsPage = new TagsPage();
+  const dashPage = new DashboardPage();
+
+  beforeEach(() => {
+    tagsPage.navigatetoDashboard();
+    dashPage.clickOnTags();
+  });
+
+  it("should ADD a new tag to the list", () => {
+    tagsPage.clickToAddNewTag();
+
+    tagsPage.enterDetailsNewTag('Tag1','descripcion del nuevo Tag');
+
+    tagsPage.clickOnSaveTag()
+
+  });
+
+  it("should EDIT a tag on the list", () => {
+    tagsPage.clickToExistingTag("Tag1");
+
+    tagsPage.editDetailsExistingTag("Tag1-Editada", "Descripcion del Tag Editado");
+
+    tagsPage.clickOnSaveTag();
+  });
+
+  it("should ADD MetaData to an existing tag on the list", () => {
+    tagsPage.clickToExistingTag("Tag1");
+
+    tagsPage.clickToMetaDataDetails()
+
+    tagsPage.addMetaDataToExistingTag("http://localhost:2368/tag/getting-started/", "Descripcion de la MetaData - Tag Editado");
+
+    tagsPage.clickOnSaveTag();
+  });
+
+  it("should DELETE a tag on the list", () => {
+    tagsPage.clickToExistingTag("Tag1");
+
+    tagsPage.clickDeleteTag();
+
+  });
+});
--- cypress/page-objects/tags-page.js
+++ cypress/page-objects/tags-page.js
@@ -1,20 +1,77 @@
-
 /// <reference types="cypress" />
 
 export class TagsPage {
 
-    navigatetoDashboard() {
-      cy.visit('http://localhost:2368/ghost/#/signin')
-      cy.get('.email.ember-text-field.gh-input.ember-view').type('pedro3087@gmail.com')
-      cy.get('.password.ember-text-field.gh-input.ember-view').type('administrador')
-      cy.get('#ember12 > span').click()
-    }
 
-    clickToAddNewTag() {
-        cy.get('.ember-view.gh-btn.gh-btn-primary').click()
-      }
+  navigatetoDashboard() {
+    cy.visit("http://localhost:2368/ghost/#/signin");
+    cy.get(".email.ember-text-field.gh-input.ember-view").type(
+      "pedro3087@gmail.com"
+    );
+    cy.get(".password.ember-text-field.gh-input.ember-view").type(
+      "administrador"
+    );
+    cy.get("#ember12 > span").click();
+  }
+
+  clickToAddNewTag() {
+    cy.get(".ember-view.gh-btn.gh-btn-primary").first().click({ force: true });
+  }
+
+  clickToExistingTag(tagText) {
+    cy.get(".gh-tag-list-name").contains(tagText).click({ force: true });
+  }
+
+  editDetailsExistingTag(tagText, tagDescr) {
+    cy.waitFor("#tag-name");
+
+    cy.get(".ember-text-field.gh-input.ember-view")
+      .first()
+      .clear({ force: true });
+    cy.get(".ember-text-field.gh-input.ember-view")
+      .first()
+      .type(tagText, { force: true });
+    cy.get("#tag-description").first().clear({ force: true });
+    cy.get("#tag-description").first().type(tagDescr, { force: true });
+  }
+
+  clickToMetaDataDetails() {
+    cy.get(".gh-expandable-title").contains('Meta data').click({ force: true });
+    cy.get(".gh-btn.gh-btn-expand").first().click({ force: true });
+  }
   
-    addTodo(todoText) {
-      cy.get('.new-todo').type(todoText + '{enter}')
-    }
+  addMetaDataToExistingTag(tagText, tagDescr) {
+    cy.waitFor("#tag-name");
+
+
+    cy.get(".ember-text-field.gh-input.ember-view")
+      .last()
+      .clear({ force: true });
+    cy.get(".ember-text-field.gh-input.ember-view")
+      .last()
+      .type(tagText, { force: true });
+    cy.get("#meta-description").first().clear({ force: true });
+    cy.get("#meta-description").first().type(tagDescr, { force: true });
+  }
+
+  clickDeleteTag(tagText) {
+    cy.get("button").contains('Delete tag').click({ force: true });
+    cy.get(".gh-btn.gh-btn-red.gh-btn-icon.ember-view").first().click({ force: true });
+  }
+
+  enterDetailsNewTag(tagText, tagDescr) {
+    cy.waitFor("#tag-name");
+    cy.get(".ember-text-field.gh-input.ember-view")
+      .first()
+      .type(tagText, { force: true });
+    cy.get("#tag-description").first().type(tagDescr, { force: true });
+  }
+
+  clickOnSaveTag() {
+    cy.get(".ember-view.gh-btn.gh-btn-primary").click();
+  }
+
+  validateNewCreatedTag(expectedText) {
+    cy.get(`.tags-list.gh-list`).should("have.text", expectedText);
+  }
 }
