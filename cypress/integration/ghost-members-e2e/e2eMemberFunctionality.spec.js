6ff20890043f2d5a5c795b32460e1d03c4869763
Roberto Carreño <r.carrenoz@uniandes.edu.co>
2021-05-16 17:49:31 -0500

    Se agrega codigo para screenshots en V3.42.5 para Members

--- cypress/integration/ghost-members-e2e/e2eMemberFunctionality.spec.js
+++ cypress/integration/ghost-members-e2e/e2eMemberFunctionality.spec.js
@@ -16,42 +16,58 @@ describe("Members Functionality - E2E scenarios", () => {
     const memberName = faker.name.firstName() + " " + faker.name.lastName();
     const memberEmail = faker.internet.email();
     const note = faker.lorem.words(10);
+    const functionality = "Members";
+    const scenario = ["1.Crear","2.Editar","3.Eliminar","4.CrearSinCorreo","5.Buscar"];
+    var step = {'step':1};
+    var cont = 0
 
     beforeEach(() => {
-        loginPage.loginAndGoToDashBoard();
-        dashPage.clickOnMember();
+        loginPage.loginAndGoToDashBoard(scenario[cont]+functionality, step);
+        dashPage.clickOnMember(scenario[cont]+functionality, step);
       });
 
-    it("Debe poder crear un nuevo miembro", () => {
-        memberPage.clickToNewMember();
-        memberPage.typeName(memberName);
-        memberPage.typeEmail(memberEmail);
-        memberPage.typeNote(note);
-        memberPage.clickSaveMember();
-    })
-
-    it("Debe poder editar un miembro", () => {
-        memberPage.clickToMember();
-        memberPage.typeName(memberName);
-        memberPage.typeNote(note);
-        memberPage.clickSaveMember();
-    })
-
-    it("Debe poder eliminar un miembro", () => {
-        memberPage.clickToMember();
-        memberPage.deleteMember();
-    })
-
-    it("Intentar crear un nuevo miembro sin correo", () => {
-        memberPage.clickToNewMember();
-        memberPage.typeName(memberName);
-        memberPage.typeNote(note);
-        memberPage.clickSaveMember();
-    })
-
-    it("Debe poder buscar un miembro", () => {
-        memberPage.searchMember(memberName);
-    })
-
-
+    context('ESCENARIO 1', function() {
+        it("Debe poder crear un nuevo miembro", () => {
+            memberPage.clickToNewMember(scenario[0]+functionality, step);
+            memberPage.typeName(memberName, scenario[0]+functionality, step);
+            memberPage.typeEmail(memberEmail, scenario[0]+functionality, step);
+            memberPage.typeNote(note, scenario[0]+functionality, step);
+            memberPage.clickSaveMember();
+            cont++;
+        })
+    });
+        
+    context('ESCENARIO 2', function() {
+        it("Debe poder editar un miembro", () => {
+            memberPage.clickToMember(scenario[1]+functionality, step);
+            memberPage.typeName(memberName, scenario[1]+functionality, step);
+            memberPage.typeNote(note, scenario[1]+functionality, step);
+            memberPage.clickSaveMember(scenario[1]+functionality, step);
+            cont++;
+        })
+    });
+        
+    context('ESCENARIO 3', function() {
+        it("Debe poder eliminar un miembro", () => {
+            memberPage.clickToMember(scenario[2]+functionality, step);
+            memberPage.deleteMember(scenario[2]+functionality, step);
+            cont++;
+        })
+    });
+        
+    context('ESCENARIO 4', function() {
+        it("Intentar crear un nuevo miembro sin correo", () => {
+            memberPage.clickToNewMember(scenario[3]+functionality, step);
+            memberPage.typeName(memberName, scenario[3]+functionality, step);
+            memberPage.typeNote(note, scenario[3]+functionality, step);
+            memberPage.clickSaveMember(scenario[3]+functionality, step);
+            cont++;
+        })
+    });
+        
+    context('ESCENARIO 5', function() {
+        it("Debe poder buscar un miembro", () => {
+            memberPage.searchMember(memberName, scenario[4]+functionality, step);
+        })
+    });
 })
