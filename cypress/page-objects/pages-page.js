bf8bee370e860c761ef7df92004c1cc8c9ea7760
Joan Blanco <EC7764F@comcel.com.co>
2021-05-16 17:18:22 -0500

    Se ajusta ruta de guardado de screenshots para pruebas de Pages

--- cypress/page-objects/pages-page.js
+++ cypress/page-objects/pages-page.js
@@ -7,12 +7,12 @@ export class PagePage {
 
     clickToNewPage(scenarioFunctionality, step) {
         cy.get(".gh-btn.gh-btn-green.ember-view").click({ force: true })
-            .then(async () => { this.takeScreenshoot ? await cy.screenshot(`${scenarioFunctionality}_v${this.version}_cypress_paso_${step['step']++}`) : null });
+            .then(async () => { this.takeScreenshoot ? await cy.screenshot(`${this.version}/${scenarioFunctionality}_cypress_paso_${step['step']++}`) : null });
     }
 
     addTitle(title, scenarioFunctionality, step) {
         cy.get(".gh-editor-title.ember-text-area.gh-input.ember-view").type(title)
-            .then(async () => { this.takeScreenshoot ? await cy.screenshot(`${scenarioFunctionality}_v${this.version}_cypress_paso_${step['step']++}`) : null });
+            .then(async () => { this.takeScreenshoot ? await cy.screenshot(`${this.version}/${scenarioFunctionality}_cypress_paso_${step['step']++}`) : null });
     }
 
     savePage() {
@@ -21,50 +21,50 @@ export class PagePage {
 
     getDraft(title, scenarioFunctionality, step) {
         cy.get('.gh-content-entry-title').contains(title).click({ force: true })
-            .then(async () => { this.takeScreenshoot ? await cy.screenshot(`${scenarioFunctionality}_v${this.version}_cypress_paso_${step['step']++}`) : null });
+            .then(async () => { this.takeScreenshoot ? await cy.screenshot(`${this.version}/${scenarioFunctionality}_cypress_paso_${step['step']++}`) : null });
     }
 
     addParagraph(text, scenarioFunctionality, step) {
         cy.get('.koenig-editor__editor.__mobiledoc-editor').click();
         cy.get('.koenig-editor__editor.__mobiledoc-editor').type(text)
-            .then(async () => { this.takeScreenshoot ? await cy.screenshot(`${scenarioFunctionality}_v${this.version}_cypress_paso_${step['step']++}`) : null });
+            .then(async () => { this.takeScreenshoot ? await cy.screenshot(`${this.version}/${scenarioFunctionality}_cypress_paso_${step['step']++}`) : null });
     }
 
     publishPage(scenarioFunctionality, step) {
         cy.get('.gh-publishmenu.ember-view').click()
-            .then(async () => { this.takeScreenshoot ? await cy.screenshot(`${scenarioFunctionality}_v${this.version}_cypress_paso_${step['step']++}`) : null });
+            .then(async () => { this.takeScreenshoot ? await cy.screenshot(`${this.version}/${scenarioFunctionality}_cypress_paso_${step['step']++}`) : null });
         cy.get('.gh-btn.gh-btn-blue.gh-publishmenu-button.gh-btn-icon.ember-view').click()
-            .then(async () => { this.takeScreenshoot ? await cy.screenshot(`${scenarioFunctionality}_v${this.version}_cypress_paso_${step['step']++}`) : null });
+            .then(async () => { this.takeScreenshoot ? await cy.screenshot(`${this.version}/${scenarioFunctionality}_cypress_paso_${step['step']++}`) : null });
     }
 
     schedulePage(scenarioFunctionality, step) {
         cy.get('.gh-publishmenu.ember-view').click()
-            .then(async () => { this.takeScreenshoot ? await cy.screenshot(`${scenarioFunctionality}_v${this.version}_cypress_paso_${step['step']++}`) : null });
+            .then(async () => { this.takeScreenshoot ? await cy.screenshot(`${this.version}/${scenarioFunctionality}_cypress_paso_${step['step']++}`) : null });
         cy.get('.gh-publishmenu-radio-label').contains('Schedule it for later').click()
-            .then(async () => { this.takeScreenshoot ? await cy.screenshot(`${scenarioFunctionality}_v${this.version}_cypress_paso_${step['step']++}`) : null });
+            .then(async () => { this.takeScreenshoot ? await cy.screenshot(`${this.version}/${scenarioFunctionality}_cypress_paso_${step['step']++}`) : null });
         cy.get('.gh-btn.gh-btn-blue.gh-publishmenu-button.gh-btn-icon.ember-view').click()
-            .then(async () => { this.takeScreenshoot ? await cy.screenshot(`${scenarioFunctionality}_v${this.version}_cypress_paso_${step['step']++}`) : null });
+            .then(async () => { this.takeScreenshoot ? await cy.screenshot(`${this.version}/${scenarioFunctionality}_cypress_paso_${step['step']++}`) : null });
     }
 
     pageWithNameExist(title, scenarioFunctionality, step) {
         cy.get('.gh-content-entry-title').contains(title).should("not.be.empty")
-            .then(async () => { this.takeScreenshoot ? await cy.screenshot(`${scenarioFunctionality}_v${this.version}_cypress_paso_${step['step']++}`) : null });
+            .then(async () => { this.takeScreenshoot ? await cy.screenshot(`${this.version}/${scenarioFunctionality}_cypress_paso_${step['step']++}`) : null });
     }
 
     deleteTitle(scenarioFunctionality, step) {
         cy.get(".gh-editor-title.ember-text-area.gh-input.ember-view").clear()
-            .then(async () => { this.takeScreenshoot ? await cy.screenshot(`${scenarioFunctionality}_v${this.version}_cypress_paso_${step['step']++}`) : null });
+            .then(async () => { this.takeScreenshoot ? await cy.screenshot(`${this.version}/${scenarioFunctionality}_cypress_paso_${step['step']++}`) : null });
     }
 
     openSettings(scenarioFunctionality, step) {
         cy.get('.post-settings').click()
-            .then(async () => { this.takeScreenshoot ? await cy.screenshot(`${scenarioFunctionality}_v${this.version}_cypress_paso_${step['step']++}`) : null });
+            .then(async () => { this.takeScreenshoot ? await cy.screenshot(`${this.version}/${scenarioFunctionality}_cypress_paso_${step['step']++}`) : null });
     }
 
     deletePage(scenarioFunctionality, step) {
         cy.get('form').find('.gh-btn.gh-btn-hover-red.gh-btn-icon.settings-menu-delete-button').contains('Delete page').click()
-            .then(async () => { this.takeScreenshoot ? await cy.screenshot(`${scenarioFunctionality}_v${this.version}_cypress_paso_${step['step']++}`) : null });
+            .then(async () => { this.takeScreenshoot ? await cy.screenshot(`${this.version}/${scenarioFunctionality}_cypress_paso_${step['step']++}`) : null });
         cy.get('.modal-footer').find('.gh-btn.gh-btn-red.gh-btn-icon.ember-view').contains('Delete').click()
-            .then(async () => { this.takeScreenshoot ? await cy.screenshot(`${scenarioFunctionality}_v${this.version}_cypress_paso_${step['step']++}`) : null });
+            .then(async () => { this.takeScreenshoot ? await cy.screenshot(`${this.version}/${scenarioFunctionality}_cypress_paso_${step['step']++}`) : null });
     }
 }
