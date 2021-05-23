d97b94ff9a3eadac037bf6d69f4add6a03c497a1
Roberto Carreño <r.carrenoz@uniandes.edu.co>
2021-05-22 20:41:58 -0500

    10 Escenarios Aleatorios/Faker para Members

--- cypress/integration/ghost-members-e2e/e2eMemberFunctionality.spec.js
+++ cypress/integration/ghost-members-e2e/e2eMemberFunctionality.spec.js
@@ -15,7 +15,9 @@ describe("Members Functionality - E2E scenarios", () => {
     const memberPage = new MemberPage();
     const memberName = faker.name.firstName() + " " + faker.name.lastName();
     const memberEmail = faker.internet.email();
+    const memberEmail2 = faker.internet.email();
     const note = faker.lorem.words(10);
+    const memberFakeName = faker.lorem.paragraph();
     const functionality = "Members";
     const scenario = ["1.Crear","2.Editar","3.Eliminar","4.CrearSinCorreo","5.Buscar"];
     var step = {'step':1};
@@ -26,7 +28,7 @@ describe("Members Functionality - E2E scenarios", () => {
         dashPage.clickOnMember(scenario[cont]+functionality, step);
       });
 
-    context('ESCENARIO 1', function() {
+    context('ESCENARIO 1 - ALEATORIO', function() {
         it("Debe poder crear un nuevo miembro", () => {
             memberPage.clickToNewMember(scenario[0]+functionality, step);
             memberPage.typeName(memberName, scenario[0]+functionality, step);
@@ -36,8 +38,20 @@ describe("Members Functionality - E2E scenarios", () => {
             cont++;
         })
     });
+
+    context('ESCENARIO 2 - ALEATORIO', function() {
+        it("No Debe poder crear un nuevo miembro por correo invalido", () => {
+            memberPage.clickToNewMember(scenario[0]+functionality, step);
+            memberPage.typeName(memberName, scenario[0]+functionality, step);
+            memberPage.typeEmail(memberEmail+memberEmail2, scenario[0]+functionality, step);
+            memberPage.typeNote(note, scenario[0]+functionality, step);
+            memberPage.clickSaveMember();
+            memberPage.validateMemberCreation();
+            cont++;
+        })
+    });
         
-    context('ESCENARIO 2', function() {
+    context('ESCENARIO 3 - ALEATORIO', function() {
         it("Debe poder editar un miembro", () => {
             memberPage.clickToMember(scenario[1]+functionality, step);
             memberPage.typeName(memberName, scenario[1]+functionality, step);
@@ -46,8 +60,42 @@ describe("Members Functionality - E2E scenarios", () => {
             cont++;
         })
     });
-        
-    context('ESCENARIO 3', function() {
+
+    context('ESCENARIO 4 - ALEATORIO', function() {
+        it("No debe poder editar un miembro por nombre invalido", () => {
+            memberPage.clickToMember(scenario[1]+functionality, step);
+            memberPage.typeName(memberFakeName, scenario[1]+functionality, step);
+            memberPage.typeNote(note, scenario[1]+functionality, step);
+            memberPage.clickSaveMember(scenario[1]+functionality, step);
+            memberPage.validateMemberEdit();
+            cont++;
+        })
+    });
+    
+    context('ESCENARIO 5 - ALEATORIO', function() {
+        it("No debe poder editar un miembro por correo invalido", () => {
+            memberPage.clickToMember(scenario[1]+functionality, step);
+            memberPage.typeName(memberName, scenario[1]+functionality, step);
+            memberPage.typeNote(note, scenario[1]+functionality, step);
+            memberPage.typeEmail(memberEmail+memberEmail2, scenario[1]+functionality, step);
+            memberPage.clickSaveMember(scenario[1]+functionality, step);
+            memberPage.validateMemberCreation();
+            cont++;
+        })
+    });
+
+    context('ESCENARIO 6 - ALEATORIO', function() {
+        it("No debe poder editar un miembro por nota invalida", () => {
+            memberPage.clickToMember(scenario[1]+functionality, step);
+            memberPage.typeName(memberName, scenario[1]+functionality, step);
+            memberPage.typeNote(memberFakeName+memberFakeName, scenario[1]+functionality, step);
+            memberPage.clickSaveMember(scenario[1]+functionality, step);
+            memberPage.validateMemberNoteEdit();
+            cont++;
+        })
+    });
+
+    context('ESCENARIO 7 - ALEATORIO', function() {
         it("Debe poder eliminar un miembro", () => {
             memberPage.clickToMember(scenario[2]+functionality, step);
             memberPage.deleteMember(scenario[2]+functionality, step);
@@ -55,7 +103,7 @@ describe("Members Functionality - E2E scenarios", () => {
         })
     });
         
-    context('ESCENARIO 4', function() {
+    context('ESCENARIO 8 - ALEATORIO', function() {
         it("Intentar crear un nuevo miembro sin correo", () => {
             memberPage.clickToNewMember(scenario[3]+functionality, step);
             memberPage.typeName(memberName, scenario[3]+functionality, step);
@@ -64,8 +112,18 @@ describe("Members Functionality - E2E scenarios", () => {
             cont++;
         })
     });
+
+    context('ESCENARIO 9 - ALEATORIO', function() {
+        it("Intentar crear un nuevo miembro sin nombre", () => {
+            memberPage.clickToNewMember(scenario[3]+functionality, step);
+            memberPage.typeEmail(memberEmail2, scenario[3]+functionality, step);
+            memberPage.clickSaveMember(scenario[3]+functionality, step);
+            memberPage.validateMemberEmailCreation(memberEmail2);
+            cont++;
+        })
+    });
         
-    context('ESCENARIO 5', function() {
+    context('ESCENARIO 10 - ALEATORIO', function() {
         it("Debe poder buscar un miembro", () => {
             memberPage.searchMember(memberName, scenario[4]+functionality, step);
         })
--- cypress/page-objects/members-page.js
+++ cypress/page-objects/members-page.js
@@ -6,23 +6,19 @@ export class MemberPage {
     takeScreenshoot = Cypress.config("takeScreenshoot");
 
     clickToNewMember(scenarioFunctionality,step) {
-        cy.get('[class="ember-view gh-btn gh-btn-green"]').first().click({ force: true })
-        .then(async()=>{this.takeScreenshoot ? await cy.screenshot(`${this.version}/${scenarioFunctionality}_cypress_paso_${step['step']++}`):null});
+        cy.get('[class="ember-view gh-btn gh-btn-green"]').first().click({ force: true });
     }
 
     typeName(name, scenarioFunctionality,step) {
-        cy.get('#member-name').type(name, { force: true })
-        .then(async()=>{this.takeScreenshoot ? await cy.screenshot(`${this.version}/${scenarioFunctionality}_cypress_paso_${step['step']++}`):null});
+        cy.get('#member-name').type(name, { force: true });
     }
 
     typeEmail(email, scenarioFunctionality,step) {
-        cy.get('#member-email').type(email, { force: true })
-        .then(async()=>{this.takeScreenshoot ? await cy.screenshot(`${this.version}/${scenarioFunctionality}_cypress_paso_${step['step']++}`):null});
+        cy.get('#member-email').type(email, { force: true });
     }
 
     typeNote(note, scenarioFunctionality,step) {
-        cy.get('#member-note').type(note, { force : true })
-        .then(async()=>{this.takeScreenshoot ? await cy.screenshot(`${this.version}/${scenarioFunctionality}_cypress_paso_${step['step']++}`):null});
+        cy.get('#member-note').type(note, { force : true });
     }
 
     clickSaveMember() {
@@ -31,25 +27,38 @@ export class MemberPage {
 
     clickToMember(scenarioFunctionality,step) {
         cy.visit('http://localhost:2368/ghost/#/members');
-        cy.get('ol>li').last().click()
-        .then(async()=>{this.takeScreenshoot ? await cy.screenshot(`${this.version}/${scenarioFunctionality}_cypress_paso_${step['step']++}`):null});
+        cy.get('ol>li').last().click();
     }
 
     deleteMember(scenarioFunctionality,step) {
-        cy.get(".gh-btn-red").first().click({ force: true })
-        .then(async()=>{this.takeScreenshoot ? await cy.screenshot(`${this.version}/${scenarioFunctionality}_cypress_paso_${step['step']++}`):null});
+        cy.get(".gh-btn-red").first().click({ force: true });
         cy.wait(500);
-        cy.get('[class="gh-btn gh-btn-red gh-btn-icon ember-view"]').click( { force: true } )
-        .then(async()=>{this.takeScreenshoot ? await cy.screenshot(`${this.version}/${scenarioFunctionality}_cypress_paso_${step['step']++}`):null});
+        cy.get('[class="gh-btn gh-btn-red gh-btn-icon ember-view"]').click( { force: true } );
     }
 
     searchMember(name, scenarioFunctionality,step) {
         cy.visit('http://localhost:2368/ghost/#/members');
-        cy.get('[class="ember-text-field gh-input ember-view gh-members-list-searchfield "]').type(name, { force : true })
-        .then(async()=>{this.takeScreenshoot ? await cy.screenshot(`${this.version}/${scenarioFunctionality}_cypress_paso_${step['step']++}`):null});
+        cy.get('[class="ember-text-field gh-input ember-view gh-members-list-searchfield "]').type(name, { force : true });
         cy.wait(500);
-        cy.get('[class="ma0 pa0 gh-members-list-name "]').should('contain', name)
-        .then(async()=>{this.takeScreenshoot ? await cy.screenshot(`${this.version}/${scenarioFunctionality}_cypress_paso_${step['step']++}`):null});
+        cy.get('[class="ma0 pa0 gh-members-list-name "]').should('contain', name);
     }
-    
+
+    validateMemberCreation(){
+        cy.get('[class="response"]').should('contain', 'Invalid Email.');
+    }
+
+    validateMemberEdit(){
+        cy.get('[class="response"]').should('contain', 'Name cannot be longer than 191 characters.');
+    }
+
+    validateMemberEmailCreation(email){
+        cy.visit('http://localhost:2368/ghost/#/members');
+        cy.get('[class="ember-text-field gh-input ember-view gh-members-list-searchfield "]').type(email, { force : true });
+        cy.wait(500);
+        cy.get('[class="ma0 pa0 gh-members-list-name gh-members-name-noname"]').should('contain', email);
+    }
+
+    validateMemberNoteEdit(){
+        cy.get('[class="response"]').should('contain', 'Note is too long.');
+    }
 }
